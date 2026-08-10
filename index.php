a6cec8c594ccb8f79bd0055aa15407b5d7d9ef3d
Mhilmy11 <setiawantojr@gmail.com>
2026-08-10 11:38:17 +0700

    fix CORS headers to avoid duplicate Access-Control-Allow-Credentials declaration

--- index.php
+++ index.php
@@ -30,9 +30,9 @@ $origin = $_SERVER['HTTP_ORIGIN'] ?? '';
 
 if (in_array($origin, $allowedOrigins, true)) {
     header("Access-Control-Allow-Origin: {$origin}");
+    header('Access-Control-Allow-Credentials: true');
 }
 
-header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
